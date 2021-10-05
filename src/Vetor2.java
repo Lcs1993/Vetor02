9e7f04dac09bc0358f32a11cbdba8c86ec0db104
Lcs1993 <ldacosta461@gmail.com>
2021-10-05 09:16:57 -0300

    exemplo vetor dias dos mês

---
+++ src/Vetor2.java
@@ -0,0 +1,20 @@
+
+public class Vetor2 {
+
+	public static void main(String[] args) {
+		String mes[]= {"jan","fev","mar","abr","mai","jun","jul",
+				"ago","set","out","nov","dez"};
+		int total[]= {31,28,31,30,31,30,31,31,30,31,30,31};
+		for(int c=0;c<mes.length;c++) {
+			System.out.println("o mês de "+mes[c]+" tem "+total[c]+" dias ao todo");
+		}
+		int anoAtual=2021;
+		if(((anoAtual%4==0 )&& (anoAtual%100!=0))||anoAtual%400==0) {
+			System.out.println("o ano atual é bisexto");
+		}else {
+			System.out.println("o ano atual não é bisexto");
+		}
+
+	}
+
+}
