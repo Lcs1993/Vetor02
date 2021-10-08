776ec4ab9a365ad63a9cc95ba18513f6cfd02cab
Lcs1993 <ldacosta461@gmail.com>
2021-10-08 08:58:23 -0300

    exemplo de preenchimento automatico para todas posições do Arrays

---
+++ src/Vetor5.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Vetor5 {
+
+	public static void main(String[] args) {
+		int vetor[]=new int[20];
+		Arrays.fill(vetor, 0);
+		for (int i : vetor) {
+			System.out.print(i +" ");
+		}
+
+	}
+
+}
